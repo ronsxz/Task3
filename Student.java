9737ed15ad06f321bd41fda05bf6c8e119675ff5
halcyon <gayojamie77@gmail.com>
2026-02-22 13:31:20 +0800

    Update Student Class
    Add getter method to Student class

--- Student.java
+++ Student.java
@@ -3,12 +3,20 @@ public class Student {
     private String firstName;
     private String lastName;
     private int age;
+    private String gender;
+    private String course;
+    private int yearLevel;
+    private String email;
 
-    public Student(int studID, String firstName, String lastName, int age){
+    public Student(int studID, String firstName, String lastName, int age, String gender, String course, int yearLevel, String email){
         this.studID = studID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.gender = gender;
+        this.course = course;
+        this.yearLevel = yearLevel;
+        this.email = email;
     } 
     public int getStudID() {
         return studID;
@@ -22,4 +30,17 @@ public class Student {
     public int getAge(){
         return age;
     }
+    public String getGender(){
+        return gender;
+    }
+    public String getCourse(){
+        return course;
+    }
+    public int getYearLevel(){
+        return yearLevel;
+    }
+    public int getEmail(){
+        return email;
+    }
+    
 }
